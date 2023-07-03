0140ace5dc946c57e35ab9d0c6b6fb0a7a8ede10
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-07-03 21:13:29 +0530

    first question and answer set

---
+++ lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:second_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs?',
+    [
+      'Widget',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'How are Flutter UIs built?',
+    [
+      'By combining widgets in code',
+      'By combining widgets in a visual editor',
+      'By defining widget in config files',
+      'By using XCode for iOS and Android Studio for Android',
+    ],
+  ),
+  QuizQuestion(
+    'What\'s the purpose of a StatefulWidget?',
+    [
+      'Update UI as data changes',
+      'Update data as UI changes',
+      'Ignore data changes',
+      'Render UI that does not depend on data',
+    ],
+  ),
+  QuizQuestion(
+    'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Both are equally good',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    'What happend if you change data in StatelessWidget?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidget are updated',
+    ],
+  ),
+  QuizQuestion(
+    'How should you update data inside of StatefulWidget?',
+    [
+      'By calling setState()',
+      'By calling updateData()',
+      'By calling updateUI()',
+      'By calling updateState()',
+    ],
+  ),
+];
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/answer_button.dart';
+import 'package:second_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +14,28 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(
+              answerText: answer,
+              onTap: () {},
+            );
+          }),
+        ],
+      ),
+    );
   }
 }
